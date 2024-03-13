22905a57036696dba5a419f9acae4b5f13a0776a
sxboyct <126867965+sxboyct@users.noreply.github.com>
2024-03-13 11:19:08 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -1,13 +1,13 @@
 import kunyu77 from './spider/video/kunyu77.js';
 import kkys from './spider/video/kkys.js';
-import wogg from './spider/video/wogg_open.js';
+import wobg from './spider/video/wobg.js';
 import push from './spider/video/push.js';
 import alist from './spider/pan/alist.js';
 import _13bqg from './spider/book/13bqg.js';
 import copymanga from './spider/book/copymanga.js';
 import ffm3u8 from './spider/video/ffm3u8.js';
 
-const spiders = [kunyu77, kkys, wogg, ffm3u8, push, alist, _13bqg, copymanga];
+const spiders = [kunyu77, kkys, wobg, ffm3u8, push, alist, _13bqg, copymanga];
 const spiderPrefix = '/spider';
 
 /**
